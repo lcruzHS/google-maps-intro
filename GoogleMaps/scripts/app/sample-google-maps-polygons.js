5cbb4dc28967a586f7c214e60f3e631ccb8117bd
lcruzHS <32069524+lcruzHS@users.noreply.github.com>
2017-10-12 17:54:48 -0400

    Reorganized JS code into modules.

---
+++ GoogleMaps/scripts/app/sample-google-maps-polygons.js
@@ -0,0 +1,96 @@
+﻿var sample = sample || {};
+sample.googleMaps = sample.googleMaps || {};
+
+sample.googleMaps.polygons = function (module) {
+
+    function drawMarkers (map) {
+        //var image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Flag_of_Wales_2.svg/255px-Flag_of_Wales_2.svg.png';
+        var centerMarker = new google.maps.Marker({
+            map: map,
+            //icon: image,
+            position: new google.maps.LatLng(52.427548, -3.515625),
+            title: "The Offa's Dyke Center, Knighton, Powys"
+        });
+
+        var pubMarker = new google.maps.Marker({
+            map: map,
+            //icon: image,
+            position: new google.maps.LatLng(52.489548, -3.515905),
+            title: 'The Knighton Hotel'
+        });
+    };
+    function drawPolyline (map) {
+        var pathCoordinates = [
+            new google.maps.LatLng(52.427548, -3.15625),
+            new google.maps.LatLng(52.520048, -3.615625),
+            new google.maps.LatLng(52.627548, -3.795625),
+            new google.maps.LatLng(52.729994, -3.815625),
+            new google.maps.LatLng(52.827548, -3.915625)
+        ];
+        var pathToCenter = new google.maps.Polyline({
+            path: pathCoordinates,
+            geodesic: false,
+            strokeColor: '#0000FF',
+            strokeOpacity: 1.0,
+            storkeWeight: 1
+        });
+        pathToCenter.setMap(map);
+    };
+    function drawPolygon (map) {
+        var natureCoords = [
+            new google.maps.LatLng(52.427548, -3.15625),
+            new google.maps.LatLng(52.520048, -3.615625),
+            new google.maps.LatLng(52.627548, -3.795625),
+            new google.maps.LatLng(52.729994, -3.815625),
+            new google.maps.LatLng(52.827548, -3.915625),
+        ];
+        var polygon = new google.maps.Polygon({
+            path: natureCoords,
+            strokeColor: '#FFFFFF',
+            strokeOpacity: 0.8,
+            storkeWeight: 1,
+            fillColor: '#00ff00',
+            fillOpacity: 0.6,
+            editable: true
+        });
+        polygon.setMap(map);
+    };
+    function drawRectangle (map) {
+        var bounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(52.827548, -3.915625),
+            new google.maps.LatLng(52.427548, -3.15625)
+        );
+        var rectangle = new google.maps.Rectangle({
+            bounds: bounds,
+            strokeColor: '#ff0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 1,
+            fillColor: '#00aa00',
+            fillOpacity: 0.6,
+            draggable: true
+        });
+        rectangle.setMap(map);
+    };
+    function drawCircle (map) {
+        var circle = new google.maps.Circle({
+            map: map,
+            center: new google.maps.LatLng(52.489548, -3.515905),
+            strokeColor: '#ff0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 1,
+            fillColor: '#00cc00',
+            fillOpacity: 0.6
+        });
+        circle.setRadius(5000); //In meters.
+        //circle.setMap(map);
+    };
+
+    module.drawMarkers = drawMarkers.bind(this);    
+    module.drawPolyline = drawPolyline.bind(this);    
+    module.drawPolygon = drawPolygon.bind(this);
+    module.drawRectangle = drawRectangle.bind(this);    
+    module.drawCircle = drawCircle.bind(this);
+
+    return module;
+
+}(sample.googleMaps.polygons || {});
---
+++ GoogleMaps/scripts/app/sample-google-maps.js
@@ -0,0 +1,51 @@
+﻿var sample = sample || {};
+sample.googleMaps = sample.googleMaps || {}; 
+
+sample.googleMaps = function(module, polygons) {
+
+    var mapOptions = {
+        center: new google.maps.LatLng(52.427548, -3.515625),
+        zoom: 8,
+        /* UI Controls settings */
+        disableDefaultUI: true,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.LARGE,
+            positon: google.maps.ControlPosition.BOTTOM_LEFT
+        },
+        panControl: true, /* Allows moving around the map and also (when zooming) enough doubles as the rotate control */
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP,
+                            google.maps.MapTypeId.HYBRID],
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        },
+        scaleControl: true,
+        streetViewControl: true, /* Allows dragging the yellow man into the map and activate a stree view */
+        rotateControl: true,
+        overViewMapControl: true,
+        overViewMapControlOptions: {
+            opened: true
+        },
+        navigationControl: true
+    };
+    var map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
+
+    module.CreateMap = function (options) {
+        if (!options || !options.container) return null;
+        
+        var map = new google.maps.Map(document.getElementById(options.container), mapOptions);
+
+        return map;
+    };
+
+    module.runSamples = function (map) {
+        polygons.drawMarkers(map);
+        polygons.drawPolyline(map);
+        polygons.drawPolygon(map);
+        polygons.drawRectangle(map);
+        polygons.drawCircle(map);
+    };
+
+    return module;
+}(sample.googleMaps || {}, sample.googleMaps.polygons);
